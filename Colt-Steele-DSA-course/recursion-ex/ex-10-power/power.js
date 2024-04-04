31a841f8b5ee199ba1633ff45daafbb67c981f64
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-04-04 19:55:29 -0400

    solve

--- Colt-Steele-DSA-course/recursion-ex/ex-10-power/power.js
+++ Colt-Steele-DSA-course/recursion-ex/ex-10-power/power.js
@@ -2,4 +2,15 @@
 // power(2,2) // 4
 // power(2,4) // 16
 
-function power() {}
+function power(base, exponent) {
+  // initialize base and exponent vars in parameters
+  // set up base case
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+
+  // return base to the power of exponent
+}
+
+console.log(power(2, 0)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(2, 4)); // 16
