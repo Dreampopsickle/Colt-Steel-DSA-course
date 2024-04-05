8e848f438a50b45ad402a600a71cb60f203d373c
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-04-04 20:10:49 -0400

    solve factorial

---
+++ Colt-Steele-DSA-course/recursion-ex/ex-11-factorial/factorial.js
@@ -0,0 +1,11 @@
+function factorial(num) {
+  // establish base case where 0! is 1
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+  // return recursive function
+}
+
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
