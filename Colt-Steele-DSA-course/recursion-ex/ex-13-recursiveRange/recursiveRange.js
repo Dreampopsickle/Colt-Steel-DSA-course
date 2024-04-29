ab10326f1fd823b0ee5090e275b4a1aad4aa8fca
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-04-29 15:39:41 -0400

    first try

--- Colt-Steele-DSA-course/recursion-ex/ex-13-recursiveRange/recursiveRange.js
+++ Colt-Steele-DSA-course/recursion-ex/ex-13-recursiveRange/recursiveRange.js
@@ -2,4 +2,20 @@
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55
 
-function recursiveRange() {}
+function recursiveRange(num) {
+  let sum;
+  function helper(helperInput) {
+    let innerSum = helperInput - 1;
+    sum += innerSum;
+    if (innerSum === 0) {
+      return;
+    }
+
+    return helper(innerSum);
+  }
+  helper(num);
+  return sum;
+}
+
+console.log(recursiveRange(6));
+console.log(recursiveRange(10));
